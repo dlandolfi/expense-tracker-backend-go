ca172bdb8192255cb90c7392fd39f5e6c0d5943d
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2026-06-25 18:20:55 -0400

    adding structured logging

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,7 +2,9 @@ package main
 
 import "net/http"
 
-func HealthHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) HealthHandler(w http.ResponseWriter, r *http.Request) {
+	app.logger.Info("request received", "uri", r.URL.RequestURI())
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"status":"ok"}`))
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", HealthHandler)
+	mux.HandleFunc("/health", app.HealthHandler)
 
 	return mux
 }
